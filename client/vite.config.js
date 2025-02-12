479e7b1cded3b0f56154f9adbb5075b4718374a3
yakov55994 <141222023+yakov55994@users.noreply.github.com>
2025-02-12 18:26:48 +0200

    s

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: './', // מציין את ספריית השורש שבה נמצא קובץ index.html
   plugins: [react()],
-  build: {
-    outDir: './dist', // מציין את תיקיית הפלט של הבנייה
-  },
-  base: './', // אם אתה מפרסם בנתיב משנה, שנה את זה לנתיב המתאים
 })
