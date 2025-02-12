4f2a8d5a9d69a4e1a45178ee5a6c6d79c793996d
yakov55994 <141222023+yakov55994@users.noreply.github.com>
2025-02-12 22:38:47 +0200

    sd

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'client/index.html',  // ודא שהנתיב הזה נכון
+      input: 'index.html',  // ודא שהנתיב הזה נכון
     }
   }
 });
