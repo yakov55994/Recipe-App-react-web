35bc0d0d7d073f31aed988de1b50a87527ded96c
yakov55994 <141222023+yakov55994@users.noreply.github.com>
2025-02-25 07:51:25 +0200

    x

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
+  root: './',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'index.html', // קובץ הכניסה לבנייה
-    },
-  },
 })
